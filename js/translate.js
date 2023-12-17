89b32d69d45ba27e71d3eb3f8cfed7cabb298061
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-12-18 00:26:29 +0100

    first commit

---
+++ js/translate.js
@@ -0,0 +1,38 @@
+let translations = {};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadTranslations();
+
+    const savedLang = localStorage.getItem('language') || 'fr';
+    translate(savedLang);
+
+    document.getElementById('languageSwitcher').value = savedLang;
+
+    //laguage icon
+    icon="img/icon/"+savedLang+".png";
+    document.getElementById('languageSwitcher').style.backgroundImage="url("+icon+")";
+
+    document.getElementById('languageSwitcher').addEventListener('change', function () {
+      
+        translate(this.value);
+        localStorage.setItem('language', this.value);
+        //laguage icon
+        icon="img/icon/"+this.value+".png";
+        document.getElementById('languageSwitcher').style.backgroundImage="url("+icon+")";
+    });
+
+        
+});
+
+async function loadTranslations() {
+    const response = await fetch('../content.json');
+    translations = await response.json();
+}
+
+function translate(lang) {
+    const elements = document.querySelectorAll("[data-i18n]");
+    elements.forEach(el => {
+        const key = el.getAttribute("data-i18n");
+        el.textContent = translations[lang][key] || el.textContent;
+    });
+}
